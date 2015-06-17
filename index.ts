4bd23bfdda75bc6f261317728587bd3eedbac927
Thierry Chatel <tchatel@gmail.com>
2015-06-17 15:43:11 +0200

    end

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import {
     bootstrap,
-    NgFor,
     Component,
     View,
-    formDirectives,
-    FormBuilder, ControlGroup,
-    Control,
+    NgFor,
+    FormBuilder,
+    ControlGroup,
     Validators,
+    formDirectives,
     ON_PUSH
-} from 'angular2/angular2';
+    } from 'angular2/angular2';
 
 //import {
 //    bootstrap,
@@ -24,89 +24,35 @@ import {
 
 
 @Component({
-    selector: 'cart-row',
-    properties: ['model', 'parentModel' ],
-    changeDetection: ON_PUSH
-})
-@View({
-    template: `
-        <div class="cart-col">{{model.product.reference}}</div>
-        <div class="cart-col">{{model.product.label}}</div>
-        <div class="cart-col">{{model.product.getPrice()}}</div>
-        <div class="cart-col">{{model.quantity}}</div>
-        <div class="cart-col">{{model.getAmount()}}</div>
-        <div class="cart-col"><button (click)="remove(model, parentModel)">Remove</button></div>
-    `
-})
-class CartRowComponent {
-    model: CartRow;
-    parentModel: Cart;
-    constructor() {
-    }
-    remove(model: CartRow, parentModel: Cart) {
-        return parentModel.remove(model);
-    }
-}
-
-@Component({
-    selector: 'cart',
-    properties: ['model']
-})
-@View({
-    template: `
-        <div class="rows">
-            <div class="cart-header">
-                <div class="cart-col">Reference</div>
-                <div class="cart-col">Label</div>
-                <div class="cart-col">Price</div>
-                <div class="cart-col">Quantity</div>
-                <div class="cart-col">Amount</div>
-                <div class="cart-col"></div>
-            </div>
-            <cart-row class="cart-row" *ng-for="#cartRow of model.rows" [model]="cartRow" [parent-model]="model"></cart-row>
-        </div>
-        <p>Model:{{model | json}}</p>
-    `,
-    directives: [NgFor, CartRowComponent]
-})
-class CartComponent {
-    model: Cart;
-    constructor() {
-    }
-}
-
-@Component({
     selector: 'cart-form',
     properties: ['model'],
     appInjector: [FormBuilder]
 })
 @View({
     template: `
-        <h2>Ajout d'une ligne</h2>
         <form [ng-form-model]="form">
-            <div ng-control-group="product">
-                <p><label>Référence <input ng-control="reference" type="text"/></label></p>
-                <p><label>Libellé <input ng-control="label" type="text"/></label></p>
-                <p><label>Prix <input ng-control="price" type="number"/></label></p>
-            </div>
-            <p><label>Quantité <input ng-control="quantity" type="number"/></label></p>
-            <p><input type="button" value="Ajouter"  (click)="add()" /></p>
+          <span ng-control-group="product">
+            <label>Reference: <input type="text" ng-control="reference"/></label>
+            <label>Label: <input type="text" ng-control="label"/></label>
+            <label>Price: <input type="number" ng-control="price"/></label>
+          </span>
+            <label>Quantity: <input type="number" ng-control="quantity"/></label>
+            <input type="button" (click)="add()" value="Add" [disabled]="!form.valid"/>
         </form>
-        <p>{{form.value|json}}</p>
     `,
     directives: [formDirectives]
 })
-class CartForm {
+class CartFormComponent {
     model: Cart;
     form: ControlGroup;
     constructor(fb: FormBuilder) {
         this.form = fb.group({
             product: fb.group({
-                label: ["", Validators.required],
                 reference: ["", Validators.required],
-                price: [undefined, Validators.required]
+                label: ["", Validators.required],
+                price: ["", Validators.required]
             }),
-            quantity: [undefined, Validators.required]
+            quantity: ["", Validators.required],
         });
     }
     add() {
@@ -126,31 +72,84 @@ class CartForm {
     }
 }
 
+
+@Component({
+    selector: 'cart-row',
+    properties: ['model', 'parentModel'],
+    changeDetection: ON_PUSH
+})
+@View({
+    template: `
+        <div class="cart-col">{{model.product.reference}}</div>
+        <div class="cart-col">{{model.product.label}}</div>
+        <div class="cart-col">{{model.product.getPrice()}}</div>
+        <div class="cart-col">{{model.quantity}}</div>
+        <div class="cart-col">{{model.getAmount()}}</div>
+        <div class="cart-col"><button (click)="remove()">Remove</button></div>
+    `,
+    directives: []
+})
+class CartRowComponent {
+    model: CartRow;
+    parentModel: Cart;
+    constructor() {
+    }
+    remove() {
+        this.parentModel.remove(this.model);
+    }
+}
+
+@Component({
+    selector: 'cart',
+    properties: ['model']
+})
+@View({
+    template: `
+        <div class="rows">
+            <div class="cart-header">
+                <div class="cart-col">Reference</div>
+                <div class="cart-col">Label</div>
+                <div class="cart-col">Price</div>
+                <div class="cart-col">Quantity</div>
+                <div class="cart-col">Amount</div>
+                <div class="cart-col"></div>
+            </div>
+            <cart-row *ng-for="#row of model.rows" [model]="row" [parent-model]="model"></cart-row>
+        </div>
+    `,
+    directives: [NgFor, CartRowComponent]
+})
+class CartComponent {
+    model: Cart;
+    constructor() {
+    }
+}
+
 @Component({
     selector: 'cart-example'
 })
 @View({
     template: `
         <p>
-            <button (click)="change()">Change</button>
             <button (click)="big()">Big</button>
+            <button (click)="change()">Change</button>
         </p>
         <cart [model]="cart"></cart>
         <cart-form [model]="cart"></cart-form>
     `,
-    directives: [CartComponent, CartForm]
+    directives: [CartComponent, CartFormComponent]
 })
 class CartExample {
     cart: Cart;
     constructor() {
         this.cart = cartBuilder.getSmall();
     }
+    big() {
+        this.cart.rows = cartBuilder.getBig().rows;
+    }
     change() {
         var n = +(this.cart.rows[1].product.label.split(' ')[1]) + 1;
         this.cart.rows[1] = new CartRow(new Product("REF02", "Product " + n, 22), 1);
-    }
-    big() {
-        this.cart.rows = cartBuilder.getBig().rows;
     }
 }
 
